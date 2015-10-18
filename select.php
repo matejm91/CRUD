3e162b069e8f076929b3b779e95ffb58ea0e2523
matejm91 <katastar.maric@gmail.com>
2015-10-18 14:40:07 +0200

    Update select.php

--- select.php
+++ select.php
@@ -1,7 +1,9 @@
 <!DOCTYPE html>
 <html>
 <body>
-
+	<head> 
+	<meta charset="utf-8">  
+	</head>
 <?php
 $servername = "localhost";
 $username = "svecomhr";
@@ -10,7 +12,8 @@ $ime_baze = "svecomhr_Skladiste-Pulvis1";
 $conn = new mysqli($servername, $username, $password, $ime_baze);
 
 if(isset($_POST['submit'])){ 
-  if(preg_match("/^[  a-zA-Z]+/", $_POST['name'])){ 
+  if(preg_match("/^[  a-zA-Z]+/", $_POST['name'])){
+  mysqli_set_charset($conn,'utf-8');	  
   $name=$_POST['name']; 
   $sql="SELECT Proizvod.*, Stanje.* FROM Proizvod, Stanje WHERE Naziv LIKE '%" . $name .  "%' AND Proizvod.ID = Stanje.ID_Proizvoda"; 
   $result=mysqli_query($conn, $sql);
@@ -28,5 +31,11 @@ if(isset($_POST['submit'])){
   } 
 $conn->close();
 ?>
+ 
+<p><body> 
+ <h3>Vrati se na pocetnu</h3> 
+    <form action="http://sve91.com.hr/index.php">
+    <input type="submit" value="OK">
+</form>
 </body>
 </html>
