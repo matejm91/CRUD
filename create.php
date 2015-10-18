f25c78158d63f371b87ee9f6347e3e753aa39d51
matejm91 <katastar.maric@gmail.com>
2015-10-18 23:43:48 +0200

    Update create.php

--- create.php
+++ create.php
@@ -1,36 +1,32 @@
 <!DOCTYPE html>
 <html>
-<body>
-	<head> 
-	<meta  http-equiv="Content-Type" content="text/html";  charset="utf-8">  
-	</head>
-<p><body> 
-	<h3>Stvori proizvod</h3>  
-	<br><form  method="POST" action="create.php" id="searchform" > 
-	<input  type="text" name="ime_stvori"> 
-	<input  type="submit" name="create" value="Create"></br> 
-	</form> 
-
+<head> 
+	<meta charset="utf-8">  
+</head>
 <?php
 $servername = "localhost";
 $username = "svecomhr";
 $password = "06Stlu6fO2";
 $ime_baze = "svecomhr_Skladiste-Pulvis1";
 $conn = new mysqli($servername, $username, $password, $ime_baze);
-
-if(isset($_POST['create'])){
-  mysqli_set_charset($conn,'utf-8');	
-  $name=$_POST['ime_stvori'];
-  mysqli_query($conn,"INSERT INTO Proizvod (Naziv) VALUES ('$name')");
-  $id=mysqli_insert_id($conn);
-  $sql="INSERT INTO Stanje (ID_Proizvoda) VALUES ('$id')";
-  if ($conn->query($sql) === TRUE) {
-    echo "bravo, unio si proizvod ".$name." koji ima ID: ".$id."<br>";
-  } else {
-    echo "Greska: " . $sql . "<br>" . $conn->error;
+mysqli_set_charset($conn,'utf-8');
+if(isset($_POST['submit'])){ 
+  if(preg_match("/^[  a-zA-Z]+/", $_POST['name'])){	  
+  $name=$_POST['name']; 
+  $sql="SELECT Proizvod.*, Stanje.* FROM Proizvod, Stanje WHERE Naziv LIKE '%" . $name .  "%' AND Proizvod.ID = Stanje.ID_Proizvoda"; 
+  $result=mysqli_query($conn, $sql);
+  if ($result->num_rows > 0) { 
+  	while($row= $result->fetch_assoc()){ 
+         $Naziv  =$row['Naziv'];  
+         $Kolicina=$row['Kolicina'];  
+  	echo  "Stanje ".$Naziv . " je " . $Kolicina . " komada!!" . "<br>";  
+  	}
+  }
+  else{ 
+  echo  "<p>Please enter a search query</p>";     
+  }
+  }
   } 
-}
-
 $conn->close();
 ?>
  
