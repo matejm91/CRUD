c1557366885d182325d78697db2d742f336a2215
matejm91 <katastar.maric@gmail.com>
2015-10-18 23:52:57 +0200

    Update create.php

--- create.php
+++ create.php
@@ -3,6 +3,15 @@
 <head> 
 	<meta charset="utf-8">  
 </head>
+
+	<body> 
+	<h3>Stvori proizvod</h3> 
+	</body>	
+		<br><form  method="POST" action="create.php" id="searchform" > 
+		<input  type="text" name="ime_stvori"> 
+		<input  type="submit" name="create" value="Create"></br> 
+		</form> 
+
 <?php
 $servername = "localhost";
 $username = "svecomhr";
@@ -10,30 +19,25 @@ $password = "06Stlu6fO2";
 $ime_baze = "svecomhr_Skladiste-Pulvis1";
 $conn = new mysqli($servername, $username, $password, $ime_baze);
 mysqli_set_charset($conn,'utf-8');
-if(isset($_POST['submit'])){ 
-  if(preg_match("/^[  a-zA-Z]+/", $_POST['name'])){	  
-  $name=$_POST['name']; 
-  $sql="SELECT Proizvod.*, Stanje.* FROM Proizvod, Stanje WHERE Naziv LIKE '%" . $name .  "%' AND Proizvod.ID = Stanje.ID_Proizvoda"; 
-  $result=mysqli_query($conn, $sql);
-  if ($result->num_rows > 0) { 
-  	while($row= $result->fetch_assoc()){ 
-         $Naziv  =$row['Naziv'];  
-         $Kolicina=$row['Kolicina'];  
-  	echo  "Stanje ".$Naziv . " je " . $Kolicina . " komada!!" . "<br>";  
-  	}
-  }
-  else{ 
-  echo  "<p>Please enter a search query</p>";     
-  }
-  }
+if(isset($_POST['create'])){	
+  $name=$_POST['ime_stvori'];
+  mysqli_query($conn,"INSERT INTO Proizvod (Naziv) VALUES ('$name')");
+  $id=mysqli_insert_id($conn);
+  $sql="INSERT INTO Stanje (ID_Proizvoda) VALUES ('$id')";
+  if ($conn->query($sql) === TRUE) {
+    echo "bravo, unio si proizvod ".$name." koji ima ID: ".$id."<br>";
+  } else {
+    echo "Greska: " . $sql . "<br>" . $conn->error;
   } 
+}
+
 $conn->close();
 ?>
  
-<p><body> 
- <h3>Vrati se na pocetnu</h3> 
-    <form action="http://sve91.com.hr/index.php">
-    <input type="submit" value="OK">
+	<body> 
+		<h3>Vrati se na pocetnu</h3>
+	</body>
+		<form action="http://sve91.com.hr/index.php">
+		<input type="submit" value="OK">
 </form>
-</body>
 </html>
