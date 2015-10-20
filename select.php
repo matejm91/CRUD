7f27f59c78aa238b5ba95119debb3424e5e1702e
matejm91 <katastar.maric@gmail.com>
2015-10-20 22:06:24 +0200

    Update select.php

--- select.php
+++ select.php
@@ -4,23 +4,25 @@
 	<meta charset="utf-8">  
 </head>
 <body>
+
 <?php 
-$servername = "*******";
-$username = "*******";
-$password = "******";
+$servername = "localhost";
+$username = "********";
+$password = "*******";
 $ime_baze = "*********";
 $conn = new mysqli($servername, $username, $password, $ime_baze);
 mysqli_query($conn, "SET character_set_results = 'utf8', character_set_client = 'utf8', character_set_connection = 'utf8', character_set_database = 'utf8', character_set_server = 'utf8'");
 if(isset($_POST['submit'])){ 
-  if(preg_match("/^[  a-zA-Z]+/", $_POST['name'])){	  
+  if($_POST['name']){	  
   $name=$_POST['name']; 
   $sql="SELECT Proizvod.*, Stanje.* FROM Proizvod, Stanje WHERE Naziv LIKE '%" . $name .  "%' AND Proizvod.ID = Stanje.ID_Proizvoda"; 
   $result=mysqli_query($conn, $sql);
   if ($result->num_rows > 0) { 
   	while($row= $result->fetch_assoc()){ 
          $Naziv  =$row['Naziv'];  
-         $Kolicina=$row['Kolicina'];  
-  	echo  "Stanje ".$Naziv . " je " . $Kolicina . " komada!!" . "<br>";  
+         $Kolicina=$row['Kolicina'];
+		 $id=$row['ID_Proizvoda'];
+  	echo "Stanje ".$Naziv . " je " . $Kolicina . " komada!!	"."<a href='update.php?value1=$id'>Promijeni</a>"."<br>";
   	}
   }
   else{ 
@@ -28,9 +30,9 @@ if(isset($_POST['submit'])){
   }
   }
   } 
+ 
 $conn->close();
 ?>
- 
 
 <h3>Vrati se na pocetnu</h3>
 <form action="http://sve91.com.hr/index.php">
