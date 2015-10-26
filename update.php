7fa761df7a5276a1da84a1ef69b4db9de7b3270c
Matej <katastar.maric@gmail.com>
2015-10-26 22:19:47 +0100

    Update update.php

--- update.php
+++ update.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+﻿<!DOCTYPE html>
 <html>
 <head> 
 	<meta charset="utf-8">  
@@ -8,31 +8,32 @@
 <h3>Promijeni stanje proizvodu</h3> 
 	
 <br><form  method="POST" form accept-charset="utf-8" action="update.php" id="searchform" > 
-<input  type="text" name="naziv" value=""> 
-<input  type="number" name="kolicina" value=""> 
-<input  type="submit" name="update" value="Promijeni količinu"></br> 
+
+<input type="number" name="second" />
+<input  type="submit" name="submit" value="Promijeni količinu"></br> 
 </form> 
 
 <?php 
 $servername = "localhost";
-$username = "*********";
+$username = "********";
 $password = "********";
 $ime_baze = "**************";
 $conn = new mysqli($servername, $username, $password, $ime_baze);
 mysqli_query($conn, "SET character_set_results = 'utf8', character_set_client = 'utf8', character_set_connection = 'utf8', character_set_database = 'utf8', character_set_server = 'utf8'");
 if(isset($_POST['submit'])){ 
-  if($_POST['kolicina']){	  
-  $kolicina=$_POST['kolicina'];
-  $id=$_GET[''];
-  $sql="UPDATE Stanje SET Kolicina=$kolicina WHERE ID_Proizvoda=$id"; 
+	if($_POST['second']){	  
+	$kolicina=$_POST['second'];
+	$id=$_GET["id"];
+	$sql="UPDATE Stanje SET Kolicina=$kolicina WHERE ID_Proizvoda=$id"; 
   if ($conn->query($sql) === TRUE) { 
   	echo "Stanje je sada " . $kolicina . " komada!!	"."<br>";
   	}
-  }
   else{ 
+  }
   echo  "<p>nest ne valja</p>";     
   }
   }
+
 $conn->close(); 
 ?>
 
