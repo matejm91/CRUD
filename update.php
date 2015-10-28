075addbbab1213d8347f7d79ea5bd047d27fac3f
Matej <katastar.maric@gmail.com>
2015-10-29 00:53:06 +0100

    Update update.php

--- update.php
+++ update.php
@@ -5,36 +5,39 @@
 </head>
 <body>
 
-<h3>Promijeni stanje proizvodu</h3> 
-	
-<br><form  method="POST" form accept-charset="utf-8" action="update.php" id="searchform" > 
-
-<input type="number" name="second" />
-<input  type="submit" name="submit" value="Promijeni količinu"></br> 
-</form> 
-
-<?php 
+<?php
 $servername = "localhost";
-$username = "********";
+$username = "*******";
 $password = "********";
-$ime_baze = "**************";
+$ime_baze = "*************";
 $conn = new mysqli($servername, $username, $password, $ime_baze);
 mysqli_query($conn, "SET character_set_results = 'utf8', character_set_client = 'utf8', character_set_connection = 'utf8', character_set_database = 'utf8', character_set_server = 'utf8'");
-if(isset($_POST['submit'])){ 
-	if($_POST['second']){	  
-	$kolicina=$_POST['second'];
-	$id=$_GET["id"];
-	$sql="UPDATE Stanje SET Kolicina=$kolicina WHERE ID_Proizvoda=$id"; 
-  if ($conn->query($sql) === TRUE) { 
-  	echo "Stanje je sada " . $kolicina . " komada!!	"."<br>";
-  	}
-  else{ 
-  }
-  echo  "<p>nest ne valja</p>";     
-  }
-  }
+if (isset($_GET['id']) ? htmlspecialchars($_GET['id']) : ''){
+	$id1=$_GET['id'];
+	$kolicina=$_POST['naziv'];
+	$sql1="SELECT Proizvod.*, Stanje.* FROM Proizvod, Stanje WHERE Proizvod.ID='$id1' AND Stanje.ID_Proizvoda='$id1'";
+	$result=mysqli_query($conn, $sql1);
+	if ($result->num_rows > 0) { 
+		while($row= $result->fetch_assoc()){ 
+       	 	 $Naziv  =$row['Naziv'];  
+       		 $Kolicina=$row['Kolicina'];
+	 	 $id=$row['ID_Proizvoda'];
+  		echo '<h3>Promijeni stanje proizvodu</h3>
 
-$conn->close(); 
+		<br><form method="post" form accept-charset="utf-8" action="update.php" id="searchform">
+	        Stanje proizvoda '. $Naziv .' je <input  type="number" name="naziv" value= '.$Kolicina.'>
+	        <input  type="submit" name="promijeni" value="Promijeni količinu">
+	        <input type="number" name="id" value= '.$id1.'></br>
+	        </form>';
+		}
+		} 
+  		else 
+		{
+		echo "Problem!!";
+		}
+}
+
+$conn->close()
 ?>
 
 <h3>Vrati se na pocetnu</h3>
