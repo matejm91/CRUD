b61e3c8489d389069b5a7f7a5d80200a042e99db
Matej <katastar.maric@gmail.com>
2015-11-06 00:37:57 +0100

    Update index.php

--- index.php
+++ index.php
@@ -4,11 +4,11 @@
 
 	<head> 
 	<meta charset="utf-8"> 
-	<title>Trazi proizvod</title> 
+	<title>Traži proizvod</title> 
 	</head> 
  
-<h3>Trazi proizvod</h3> 
-	<p>Pretraga moguca po imenu</p> 
+<h3>Traži proizvod</h3> 
+	<p>Pretraga moguća po imenu</p> 
 	<form  method="post" form accept-charset="utf-8" action="select.php" id="searchform" > 
 	<input  type="text" name="name"> 
 	<input  type="submit" name="submit" value="Search"> 
@@ -20,17 +20,12 @@
 	<form action="http://sve91.com.hr/create.php">
 	<input type="submit" value="Novi proizvod">
 	</form>
-
-<h3>Promijeni proizvod</h3> 
-	<form action="http://sve91.com.hr/update.php">
-	<input type="submit" value="Promijeni proizvod">
-	</form>
 <br>
 <?php
 $servername = "localhost";
-$username = "**********";
-$password = "*******";
-$ime_baze = "***********";
+$username = "svecomhr";
+$password = "06Stlu6fO2";
+$ime_baze = "svecomhr_Skladiste-Pulvis1";
 $conn = new mysqli($servername, $username, $password, $ime_baze);
 mysqli_query($conn, "SET character_set_results = 'utf8', character_set_client = 'utf8', character_set_connection = 'utf8', character_set_database = 'utf8', character_set_server = 'utf8'");
 $sql="SELECT Proizvod.*, Stanje.* FROM Proizvod, Stanje WHERE Naziv LIKE '%" . $name .  "%' AND Proizvod.ID = Stanje.ID_Proizvoda"; 
@@ -38,14 +33,15 @@ $sql="SELECT Proizvod.*, Stanje.* FROM Proizvod, Stanje WHERE Naziv LIKE '%" . $
   if ($result->num_rows > 0) { 
   	while($row= $result->fetch_assoc()){ 
          $Naziv  =$row['Naziv'];  
-         $Kolicina=$row['Kolicina'];
+         $kolicina=$row['Kolicina'];
 	 $id=$row['ID_Proizvoda'];
-	 	echo "Stanje ".$Naziv . " je " . $Kolicina . " komada!!	"."<a href='http://www.sve91.com.hr/update.php?id=$id'>Promijeni</a> "." <a href=\"http://sve91.com.hr/delete.php\">iksić</a>"."<br>";
+	 	echo "Stanje ".$Naziv . " je " . $kolicina . " komada	"."<a href=\"http://sve91.com.hr/update.php?id=". $id ."\">Promijeni</a> "." <a href=\"http://sve91.com.hr/delete.php?id=". $id ."\">Obriši proizvod</a>"."<br>";
   	}
   }
   else{ 
-  echo  "<p>Please enter a search query</p>";     
+  echo  "<p>Molimo unesite upit za pretraživanje</p>";     
   }
+;
 $conn->close();
 ?> 
 	
