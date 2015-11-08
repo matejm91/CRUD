d1faac995849bbd1388111c8e1aebf5a84b76432
Matej <katastar.maric@gmail.com>
2015-11-08 15:11:10 +0100

    Update create.php

--- create.php
+++ create.php
@@ -7,32 +7,12 @@
 	
 <h3>Stvori proizvod</h3> 
 	
-<br><form  method="POST" form accept-charset="utf-8" action="create.php" id="searchform" > 
+<br><form  method="POST" form accept-charset="utf-8" action="create2.php" id="searchform" > 
 <input  type="text" name="ime_stvori"> 
-<input  type="submit" name="create" value="Create"></br> 
+<input  type="submit" name="create" value="Stvori"></br> 
 </form> 
 
-<?php 
-$servername = "localhost";
-$username = "**********";
-$password = "**********";
-$ime_baze = "********************";
-$conn = new mysqli($servername, $username, $password, $ime_baze);
-mysqli_query($conn, "SET character_set_results = 'utf8', character_set_client = 'utf8', character_set_connection = 'utf8', character_set_database = 'utf8', character_set_server = 'utf8'");
-if(isset($_POST['create'])){	
-  $name=$_POST['ime_stvori'];
-  mysqli_query($conn,"INSERT INTO Proizvod (Naziv) VALUES ('$name')");
-  $id=mysqli_insert_id($conn);
-  $sql="INSERT INTO Stanje (ID_Proizvoda) VALUES ('$id')";
-  if ($conn->query($sql) === TRUE) {
-    echo "Unijeli ste proizvod ".$name." koji ima ID: ".$id."<br>";
-  } else {
-    echo "Greska: " . $sql . "<br>" . $conn->error;
-  } 
-}
 
-$conn->close();
-?>
 <a href="http://sve91.com.hr/index.php">Vrati se na početnu stranicu</a>
 </body>
 </html>
