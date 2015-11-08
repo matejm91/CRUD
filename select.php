e517222f15ec0330352cc9916b0dc8bc08f3f725
Matej <katastar.maric@gmail.com>
2015-11-08 15:15:14 +0100

    Update select.php

--- select.php
+++ select.php
@@ -7,9 +7,9 @@
 
 <?php 
 $servername = "localhost";
-$username = "**********";
-$password = "**********";
-$ime_baze = "********************";
+$username = "***********";
+$password = "***********";
+$ime_baze = "**********************";
 $conn = new mysqli($servername, $username, $password, $ime_baze);
 mysqli_query($conn, "SET character_set_results = 'utf8', character_set_client = 'utf8', character_set_connection = 'utf8', character_set_database = 'utf8', character_set_server = 'utf8'");
 if(isset($_POST['submit'])){ 
@@ -18,14 +18,28 @@ if(isset($_POST['submit'])){
   $sql="SELECT Proizvod.*, Stanje.* FROM Proizvod, Stanje WHERE Naziv LIKE '%" . $name .  "%' AND Proizvod.ID = Stanje.ID_Proizvoda"; 
   $result=mysqli_query($conn, $sql);
   if ($result->num_rows > 0) { 
-  	while($row= $result->fetch_assoc()){ 
-         $Naziv  =$row['Naziv'];  
-         $Kolicina=$row['Kolicina'];
-	 $id=$row['ID_Proizvoda'];
-  	echo "Stanje ".$Naziv . " je " . $Kolicina . " komada	"."<a href='http://www.sve91.com.hr/update.php?id=$id'>Promijeni</a>"." <a href=\"http://sve91.com.hr/delete.php\">iksić</a>"."<br>";
-  	}
-  }
-  else{ 
+  	$Naziv  =$row['Naziv'];  
+        $kolicina=$row['Kolicina'];
+	$id=$row['ID_Proizvoda'];
+	 	echo "<table border = 1>";
+		echo "<tr>
+         <td>Naziv proizvoda</td>
+         <td>Količina</td>
+         <td></td>
+         <td></td>
+   </tr>";
+		while($row = mysqli_fetch_array($result)){
+		 echo "<tr>
+          <td>" . $row['Naziv'] . "</td>
+          <td>" . $row['Kolicina'] . "</td>
+          <td>" . "<a href=\"http://sve91.com.hr/update.php?id=". $row['ID_Proizvoda'] ."\">Promijeni</a>" . "</td>
+          <td>" . " <a href=\"http://sve91.com.hr/delete.php?id=". $row['ID_Proizvoda'] ."\">Obriši proizvod</a>" . "</td>
+           
+   </tr>";
+		}
+
+		 echo "</table>";
+  		 }else{ 
   echo  "<p>Molimo unesite upit za pretraživanje</p>";     
   }
   }
