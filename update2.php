0ad9d128c31e677c1a7336eb860a80419b5cb704
Matej <katastar.maric@gmail.com>
2015-11-12 13:19:32 +0100

    Update update2.php

--- update2.php
+++ update2.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "localhost";
-$username = "**********";
-$password = "**********";
-$ime_baze = "********************";
+$username = "*********";
+$password = "*********";
+$ime_baze = "***************************";
 $conn = new mysqli($servername, $username, $password, $ime_baze);
 mysqli_query($conn, "SET character_set_results = 'utf8', character_set_client = 'utf8', character_set_connection = 'utf8', character_set_database = 'utf8', character_set_server = 'utf8'");
 if (isset($_POST['prom'])){	
@@ -10,7 +10,7 @@ if (isset($_POST['prom'])){
 	if ($_POST['id']){
 	$id=$_POST['id'];
 	$Kolicina=$_POST['kolicina'];
-		if (!preg_match('/^[0-9]*$/', $Kolicina)) {
+		if (is_numeric($Kolicina) and ($Kolicina < 0)) {
 			echo "<br>"."Unešena vrijednost je: ".(filter_var($Kolicina, FILTER_SANITIZE_STRING))."</br>", "Unijeli ste nemoguću vrijednost" . '<br><a href="http://sve91.com.hr/index.php">Vrati se na početnu stranicu</a></br>';
 			} else {
 			$result1=mysqli_query($conn, "UPDATE Stanje SET Kolicina = '$Kolicina' WHERE ID_Proizvoda = '$id' ");
