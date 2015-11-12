00b2eba5368f1f4f587ec55cc2b72d81599de429
Matej <katastar.maric@gmail.com>
2015-11-12 12:48:07 +0100

    Update update2.php

--- update2.php
+++ update2.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "localhost";
-$username = "*********";
-$password = "*********";
-$ime_baze = "******************";
+$username = "**********";
+$password = "**********";
+$ime_baze = "********************";
 $conn = new mysqli($servername, $username, $password, $ime_baze);
 mysqli_query($conn, "SET character_set_results = 'utf8', character_set_client = 'utf8', character_set_connection = 'utf8', character_set_database = 'utf8', character_set_server = 'utf8'");
 if (isset($_POST['prom'])){	
@@ -11,13 +11,11 @@ if (isset($_POST['prom'])){
 	$id=$_POST['id'];
 	$Kolicina=$_POST['kolicina'];
 		if (!preg_match('/^[0-9]*$/', $Kolicina)) {
-        		echo "Unijeli ste nemoguću vrijednost" . '<br><a href="http://sve91.com.hr/index.php">Vrati se na početnu stranicu</a></br>';
-	}
-	$result1=mysqli_query($conn, "UPDATE Stanje SET Kolicina = '$Kolicina' WHERE ID_Proizvoda = '$id' ");
-	header('Location:index.php?updated=true'); 
-	}
-	else{
-	echo "Nešto nije u redu";
+			echo "<br>"."Unešena vrijednost je: ".(filter_var($Kolicina, FILTER_SANITIZE_STRING))."</br>", "Unijeli ste nemoguću vrijednost" . '<br><a href="http://sve91.com.hr/index.php">Vrati se na početnu stranicu</a></br>';
+			} else {
+			$result1=mysqli_query($conn, "UPDATE Stanje SET Kolicina = '$Kolicina' WHERE ID_Proizvoda = '$id' ");
+			header('Location:index.php?updated=true');
+}
 }
 }
 }	
