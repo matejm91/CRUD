cfa078de7119febcb9bc4748cfacfd505541e15e
Matej <katastar.maric@gmail.com>
2015-11-23 13:30:35 +0100

    Update create.php

--- create.php
+++ create.php
@@ -1,18 +1,35 @@
 <!DOCTYPE html>
 <html>
-<head> 
-	<meta charset="utf-8">  
-</head>
+<head>
+	<style>
+	.obavijest {	
+	}
+	</style>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script> 
+	<meta charset="utf-8"> 
+	<title>Traži proizvod</title> 
+	<meta name ="viewport" content="width=device-width, intital-scale=1.0">
+	<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/coustom.css">
+</head> 
 <body>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+
 	
-<h3>Stvori proizvod</h3> 
-	
-<br><form  method="POST" form accept-charset="utf-8" action="create2.php" id="searchform" > 
-<input  type="text" name="ime_stvori"> 
-<input  type="submit" name="create" value="Stvori"></br> 
-</form> 
+<br><form action="create2.php" method="post" form accept-charset="utf-8">
+		<div class="form-group">
+    			<label for="stvaranje"><h3>Stvori proizvod</h3></label>
+    			<input type="text" class="form-control" id="stvaranje" placeholder="Ime novog proizvoda" name="ime_stvori">
+  		</div>
+		<button type="submit" name="create" class="btn btn-primary">Stvori</button>
+	 </form> 
 
 
-<a href="http://sve91.com.hr/index.php">Vrati se na početnu stranicu</a>
+<br><a href="http://sve91.com.hr/products.php" class="btn btn-primary">Vrati se na početnu stranicu</a></br>
+			</div>
+		</div>
+	</div>
 </body>
 </html>
