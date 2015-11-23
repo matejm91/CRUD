03e958b845f5de740e314ccc721b38ea4e93d621
Matej <katastar.maric@gmail.com>
2015-11-23 13:38:41 +0100

    Update update2.php

--- update2.php
+++ update2.php
@@ -1,23 +1,29 @@
 <?php
-$servername = "localhost";
-$username = "*********";
-$password = "*********";
-$ime_baze = "***************************";
-$conn = new mysqli($servername, $username, $password, $ime_baze);
-mysqli_query($conn, "SET character_set_results = 'utf8', character_set_client = 'utf8', character_set_connection = 'utf8', character_set_database = 'utf8', character_set_server = 'utf8'");
-if (isset($_POST['prom'])){	
-	if ($_POST['kolicina']){
-	if ($_POST['id']){
-	$id=$_POST['id'];
-	$Kolicina=$_POST['kolicina'];
-		if (is_numeric($Kolicina) and ($Kolicina < 0)) {
-			echo "<br>"."Unešena vrijednost je: ".(filter_var($Kolicina, FILTER_SANITIZE_STRING))."</br>", "Unijeli ste nemoguću vrijednost" . '<br><a href="http://sve91.com.hr/index.php">Vrati se na početnu stranicu</a></br>';
-			} else {
-			$result1=mysqli_query($conn, "UPDATE Stanje SET Kolicina = '$Kolicina' WHERE ID_Proizvoda = '$id' ");
-			header('Location:index.php?updated=true');
-}
-}
-}
-}	
-$conn->close()
+	$servername = "localhost";
+	$username = '**********';
+	$password = "**********";
+	$ime_baze = "********************";
+	$conn = new mysqli($servername, $username, $password, $ime_baze);
+	mysqli_query($conn, "SET character_set_results = 'utf8', character_set_client = 'utf8', character_set_connection = 'utf8', character_set_database = 'utf8', character_set_server = 'utf8'");
+	if (isset($_POST['prom'])){	
+		$id=$_POST['id'];
+		$Kolicina=$_POST['kolicina'];
+		if (is_numeric($Kolicina)) {
+			if ($Kolicina < 0) {
+				echo "<br>"."Unešena vrijednost je: ".(filter_var($Kolicina, FILTER_SANITIZE_STRING))."</br>", "Unijeli ste nemoguću vrijednost" . '<br><a href="http://sve91.com.hr/products.php">Vrati se na početnu stranicu</a></br>';
+			} 
+			else {
+				$sql = "UPDATE Stanje SET Kolicina = '$Kolicina' WHERE ID_Proizvoda = '$id'";
+				if ($conn->query($sql) === TRUE) {
+					header('Location:products.php?updated=true');
+				} else {
+					echo "Greška: " . $sql . "<br>" . $conn->error;
+				} 
+			}
+		}
+		else {
+			echo "Niste unijeli broj.";
+		}
+	}	
+	$conn->close();
 ?>
