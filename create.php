a8432ba81b4d99cce6ad833a8165f7496a8cc3fa
Matej <katastar.maric@gmail.com>
2015-11-25 14:54:03 +0100

    Update create.php

--- create.php
+++ create.php
@@ -15,19 +15,17 @@
 <body>
 	<div class="container">
 		<div class="row">
-			<div class="col-md-6">
-
-	
-<br><form action="create2.php" method="post" form accept-charset="utf-8">
-		<div class="form-group">
-    			<label for="stvaranje"><h3>Stvori proizvod</h3></label>
-    			<input type="text" class="form-control" id="stvaranje" placeholder="Ime novog proizvoda" name="ime_stvori">
-  		</div>
-		<button type="submit" name="create" class="btn btn-primary">Stvori</button>
-	 </form> 
+			<div class="col-md-6">	
+				<br><form action="/test/create2.php" method="post" form accept-charset="utf-8">
+					<div class="form-group">
+    						<label for="stvaranje"><h3>Stvori proizvod</h3></label>
+    						<input type="text" class="form-control" id="stvaranje" placeholder="Ime novog proizvoda" name="ime_stvori">
+  					</div>
+					<button type="submit" name="create" class="btn btn-primary">Stvori</button>
+	 			</form> 
 
 
-<br><a href="http://sve91.com.hr/products.php" class="btn">Vrati se na početnu stranicu</a></br>
+<br><a href="http://sve91.com.hr/test/products.php" class="btn">Vrati se na početnu stranicu</a></br>
 			</div>
 		</div>
 	</div>
