459d6f10dea9856ec558f169a93208835256c93f
Matej <katastar.maric@gmail.com>
2015-11-25 14:59:16 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,32 +1,32 @@
 <?php
-  $user = "";
-  $pass = "";
-  $validated = false;
+	$user = "";
+	$pass = "";
+	$validated = false;
 
-  if ($_POST)
-  {
-    $user = $_POST['username'];
-    $pass = $_POST['password'];
-  }
+	if ($_POST)
+	{
+		$user = $_POST['username'];
+	$pass = $_POST['password'];
+	}
 
-  session_start();
-  if($user!=""&&$pass!="")
-  {
-    if($user=="*******"&&$pass=="************") $validated = true;
-    if($validated)
-    {
-      $_SESSION['login'] = "OK";
-      $_SESSION['username'] = $user;
-      $_SESSION['password'] = $pass;
-      header('Location: products.php');
-    }
-    else
-    {
-      $_SESSION['login'] = "";
-      echo "Invalid username or password.";
-    }
-  }
-  else $_SESSION['login'] = "";
+	session_start();
+	if($user!=""&&$pass!="")
+	{
+		if($user=="CRUD"&&$pass=="letmein") $validated = true;
+  		if($validated)
+		{
+			$_SESSION['login'] = "OK";
+			$_SESSION['username'] = $user;
+			$_SESSION['password'] = $pass;
+			header('Location: /test/products.php');
+		}
+		else
+		{
+			$_SESSION['login'] = "";
+			echo "Invalid username or password.";
+		}
+	}
+	else $_SESSION['login'] = "";
 ?>
 <!DOCTYPE html>
 <html>
@@ -44,20 +44,19 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-md-4">
-	<h1>Prijavi se</h1>
-	<br><p>Molimo unesite korisničko ime i šifru:</p></br>
-	
-<form action="index.php" method="post">
-  <div class="form-group">
-    <label for="exampleInputEmail1">Username</label>
-    <input type="text" class="form-control" id="exampleInputEmail1" name="username" placeholder="Username">
-  </div>
-  <div class="form-group">
-    <label for="exampleInputPassword1">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1" name="password" placeholder="Password">
-  </div>
-  <button type="submit" class="btn btn-primary">Prijavi se</button>
-</form>
+				<h1>Prijavi se</h1>
+				<br><p>Molimo unesite korisničko ime i šifru:</p></br>
+				<form action="/test/index.php" method="post">
+					<div class="form-group">
+						<label for="exampleInputEmail1">Username</label>
+						<input type="text" class="form-control" id="exampleInputEmail1" name="username" placeholder="Username">
+					</div>
+					<div class="form-group">
+						<label for="exampleInputPassword1">Password</label>
+						<input type="password" class="form-control" id="exampleInputPassword1" name="password" placeholder="Password">
+					</div>
+				<button type="submit" class="btn btn-primary">Prijavi se</button>
+				</form>
 
 			</div>
 		</div>
