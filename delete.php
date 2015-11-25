5134de784d04cb3ccb29ac9fe3c833d65c4a8485
Matej <katastar.maric@gmail.com>
2015-11-25 14:56:30 +0100

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,19 +1,19 @@
 <?php
-$servername = "localhost";
-$username = "********";
-$password = "********";
-$ime_baze = "****************";
-$conn = new mysqli($servername, $username, $password, $ime_baze);  
-$id = $_GET['id'];
-
+	$servername = "localhost";
+	$username = "**********";
+	$password = "**********";
+	$ime_baze = "********************";
+	$conn = new mysqli($servername, $username, $password, $ime_baze);  
+	$id = $_GET['id'];
 	mysqli_query($conn, "DELETE FROM Stanje WHERE ID_Proizvoda = '$id' ");
 	$query="DELETE FROM Proizvod WHERE ID = '$id'";
 	$result=mysqli_query($conn, $query);	
 	if ($result === TRUE){
-	header('Location:products.php?deleted=true');
+		header('Location:/test/products.php?deleted=true');
 	}
-	else{
-	echo "Nešto nije u redu!";
-}
-$conn->close();
+	else
+	{
+		echo "Nešto nije u redu!";
+	}
+	$conn->close();
 ?> 
